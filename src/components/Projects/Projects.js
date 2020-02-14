cdbc9c7706da55585aa3cf85b7255dcc3e8b0b50
Xander Krause <sk8rak@gmail.com>
2020-02-14 14:42:48 -0800

    Moved projects array out of this.state, and added buttons for filter testing

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,41 +2,42 @@ import React from 'react';
 import './Projects.css';
 import Pic from "../../img/yellow-and-black-striped-3662579.jpg";
 import Card from '../Card/Card';
-import Skill from '../Skill/Skill';
+import Skills from '../Skills/Skills';
+
+const projects = [
+    {
+        "id": "1",
+        "title": "Jamming",
+        "image": Pic,
+        "text": "This connects to the Spotify API, a Codecademy Project"
+    },
+    {
+        "id": "2",
+        "title": "Ravenous",
+        "image": Pic,
+        "text": "This connects to the Yelp API, a Codecademy Project"
+    },
+    {
+        "id": "3",
+        "title": "Odin's Vault",
+        "image": Pic,
+        "text": "Comic book db, to be built"
+    }
+]
 
 class Projects extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            projects: [
-                {
-                    "id": "1",
-                    "title": "Jamming",
-                    "image": Pic,
-                    "text": "This connects to the Spotify API, a Codecademy Project"
-                },
-                {
-                    "id": "2",
-                    "title": "Ravenous",
-                    "image": Pic,
-                    "text": "This connects to the Yelp API, a Codecademy Project"
-                },
-                {
-                    "id": "3",
-                    "title": "Odin's Vault",
-                    "image": Pic,
-                    "text": "Comic book db, to be built"
-                }
-            ]
+            projects
         };
 
-        /*this.onShowAll = this.onShowAll.bind(this);
+        this.onShowAll = this.onShowAll.bind(this);
         this.onClear = this.onClear.bind(this);
-        this.onShowReact = this.onShowReact.bind(this);*/
     }
 
-    /*onShowAll = () => {
+    onShowAll = () => {
         this.setState({projects});
     };
 
@@ -44,13 +45,13 @@ class Projects extends React.Component {
         this.setState({projects: []});
     };
 
-    onShowReact = () => {
-        this.setState({projects: [this.state.projects.project1]});
-    };*/
-
     render() {
         return (
             <div>
+                <div className = "container-fluid d-flex justify-content-left">
+                    <button type = "button" onClick = {this.onShowAll}>All</button>
+                    <button type = "button" onClick = {this.onClear}>Clear</button>
+                </div>
                 <div className = "container-fluid d-flex justify-content-center">
                     {
                         this.state.projects.map(project => {
