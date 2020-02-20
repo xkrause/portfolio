35b2f2a7a377bace1bd92b2634e9987ff25d747b
xkrause <sk8rak@gmail.com>
2020-02-20 12:35:47 -0800

    Rendering multiple Card components for now instead of using map

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,7 +4,7 @@ import Pic from "../../img/yellow-and-black-striped-3662579.jpg";
 import Card from '../Card/Card';
 import Skill from '../Skill/Skill';
 
-const projects = [
+/*const projects = [
     {
         "id": "1",
         "title": "Jammming",
@@ -29,14 +29,37 @@ const projects = [
         "image": Pic,
         "text": "Resume-reviewing service with different purchase plans"
     }
-];
+];*/
 
 class Projects extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            projects,
+            //projects,
+
+            projects: {
+                project1: {
+                    title: "Jammming",
+                    image: Pic,
+                    text: "This connects to the Spotify API, a Codecademy Project"
+                },
+                project2: {
+                    title: "Ticketing System",
+                    image: Pic,
+                    text: "This was a ticketing system made for Green River College"
+                },
+                project3: {
+                    title: "Auburn City Street Mission",
+                    image: Pic,
+                    text: "Website for homeless outreach organization"
+                },
+                project4: {
+                    title: "ReviewMyResume",
+                    image: Pic,
+                    text: "Resume-reviewing service with different purchase plans"
+                }
+            },
 
             skill_1: "All",
             skill_2: "React.js",
@@ -47,13 +70,13 @@ class Projects extends React.Component {
             skill_7: "Photoshop"
         };
 
-        this.onShowAll = this.onShowAll.bind(this);
+        /*this.onShowAll = this.onShowAll.bind(this);
         this.onShowReact = this.onShowReact.bind(this);
         this.onShowBootstrap = this.onShowBootstrap.bind(this);
-        this.onShowSchoolProjects = this.onShowSchoolProjects.bind(this);
+        this.onShowSchoolProjects = this.onShowSchoolProjects.bind(this);*/
     }
 
-    onShowAll = () => {
+    /*onShowAll = () => {
         this.setState({projects});
     }
 
@@ -91,12 +114,12 @@ class Projects extends React.Component {
                 "text": "Resume-reviewing service"
             }
         ]});
-    }
+    }*/
 
     render() {
         return (
             <div>
-                <div className = "container-fluid d-flex">
+                {/*<div className = "container-fluid d-flex">
                     {
                         this.state.projects.map(project => {
                             return <Card key = {project.id}
@@ -105,22 +128,36 @@ class Projects extends React.Component {
                                             text = {project.text} />
                         })
                     }
+                </div>*/}
+                <div className = "row">
+                    <div className = "col-lg-3 col-md-3">
+                        <Card title = {this.state.projects.project1.title} image = {this.state.projects.project1.image} text = {this.state.projects.project1.text} />
+                    </div>
+                    <div className = "col-lg-3 col-md-3">
+                        <Card title = {this.state.projects.project2.title} image = {this.state.projects.project2.image} text = {this.state.projects.project2.text} />
+                    </div>
+                    <div className = "col-lg-3 col-md-3">
+                        <Card title = {this.state.projects.project3.title} image = {this.state.projects.project3.image} text = {this.state.projects.project3.text} />
+                    </div>
+                    <div className = "col-lg-3 col-md-3">
+                        <Card title = {this.state.projects.project4.title} image = {this.state.projects.project4.image} text = {this.state.projects.project4.text} />
+                    </div>
                 </div>
                 <div className = "container-fluid d-flex justify-content-left">
                     <span className = "badge badge-pill badge-secondary">React.js</span>
                     <span className = "badge badge-pill badge-secondary">JavaScript</span>
                     <span className = "badge badge-pill badge-secondary">Bootstrap</span>
                     <span className = "badge badge-pill badge-secondary">HTML/CSS</span>
-                    {/*<div className = "col-lg-10">
-                        <Skill skill = {this.state.skill_1} onClick = {this.onShowAll} />
-                        <Skill skill = {this.state.skill_2} onClick = {this.onShowReact} />
-                        <Skill skill = {this.state.skill_3} onClick = {this.onShowBootstrap} />
-                        <Skill skill = {this.state.skill_4} />
-                        <Skill skill = {this.state.skill_5} onClick = {this.onShowSchoolProjects} />
-                        <Skill skill = {this.state.skill_6} />
-                        <Skill skill = {this.state.skill_7} />
-        </div>*/}
                 </div>
+                {/*<div className = "col-lg-10">
+                    <Skill skill = {this.state.skill_1} onClick = {this.onShowAll} />
+                    <Skill skill = {this.state.skill_2} onClick = {this.onShowReact} />
+                    <Skill skill = {this.state.skill_3} onClick = {this.onShowBootstrap} />
+                    <Skill skill = {this.state.skill_4} />
+                    <Skill skill = {this.state.skill_5} onClick = {this.onShowSchoolProjects} />
+                    <Skill skill = {this.state.skill_6} />
+                    <Skill skill = {this.state.skill_7} />
+                </div>*/}
             </div>
         );
     }
